7cb7785741676b038792ef47efaacd4dc7728cb8
Joe Hinkle <joehinkle11@gmail.com>
2019-12-21 12:42:04 -0500

    basic protocol

--- Sources/App-Maker/App_Maker.swift
+++ Sources/App-Maker/App_Maker.swift
@@ -1,3 +1,19 @@
-struct App_Maker {
-    var text = "Hello, World!"
+protocol AMComponent {
+    /// Name displayed in the component section of App Maker's editor
+    var editorName: String { get }
+    /// Icon displayed in the component section of App Maker's editor
+    var editorIcon: Image { get }
+    /// Description displayed in the component section of App Maker's editor
+    var editorDescription: String { get }
 }
+
+/// Not supported yet...
+protocol AMAction {
+    /// Name displayed in the component section of App Maker's editor
+    var editorName: String { get }
+    /// Icon displayed in the component section of App Maker's editor
+    var editorIcon: Image { get }
+    /// Description displayed in the component section of App Maker's editor
+    var editorDescription: String { get }
+}
+
