bedb32ede619df230e3b37f97d8a4f666040bfe3
Joe Hinkle <joehinkle11@gmail.com>
2019-12-21 12:49:14 -0500

    name change

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "App-Maker",
+    name: "AppMaker",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "App-Maker",
-            targets: ["App-Maker"]),
+            name: "AppMaker",
+            targets: ["AppMaker"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "App-Maker",
+            name: "AppMaker",
             dependencies: []),
     ]
 )
