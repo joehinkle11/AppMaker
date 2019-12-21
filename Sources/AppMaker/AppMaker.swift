8dee48b2c64813ff13a20767f291579fde227395
Joe Hinkle <joehinkle11@gmail.com>
2019-12-21 13:10:40 -0500

    static

--- Sources/AppMaker/AppMaker.swift
+++ Sources/AppMaker/AppMaker.swift
@@ -2,22 +2,22 @@ import SwiftUI
 
 public protocol AMComponent {
     /// Name displayed in the component section of App Maker's editor
-    var editorName: String { get }
+    static var editorName: String { get }
     /// Icon displayed in the component section of App Maker's editor
     @available(iOS 13.0, *)
-    var editorIcon: Image { get }
+    static var editorIcon: Image { get }
     /// Description displayed in the component section of App Maker's editor
-    var editorDescription: String { get }
+    static var editorDescription: String { get }
 }
 
 /// Not supported yet...
 public protocol AMAction {
     /// Name displayed in the component section of App Maker's editor
-    var editorName: String { get }
+    static var editorName: String { get }
     /// Icon displayed in the component section of App Maker's editor
     @available(iOS 13.0, *)
-    var editorIcon: Image { get }
+    static var editorIcon: Image { get }
     /// Description displayed in the component section of App Maker's editor
-    var editorDescription: String { get }
+    static var editorDescription: String { get }
 }
 
